0f2545a4ff85573c095a9ee05f547bb9d2b4a1b8
Martin Zylauv Kristensen <mzk@onlimited.dk>
2022-02-21 13:53:33 +0100

    fixed

--- src/components/Facility.vue
+++ src/components/Facility.vue
@@ -13,21 +13,24 @@
     </div>
 
     <div class="card-text">
-      <p><b>Organisation:</b> {{getOrganisation}}</p>
-      <p><b>Udbydertype:</b> {{getProviders}}</p>
-      <p><b>Kategori(er):</b> {{getCategories}}</p>
-      <p><b>Område(r):</b> {{getAreas}}</p>
+      <p><b>Organisation:</b> {{ getOrganisation }}</p>
+      <p><b>Udbydertype:</b> {{ getProviders }}</p>
+      <p><b>Kategori(er):</b> {{ getCategories }}</p>
+      <p><b>Område(r):</b> {{ getAreas }}</p>
     </div>
 
     <div v-if="showMore" class="card-text">
-      <p><b>Service(s):</b> {{getServices}}</p>
-      <p><b>Beskrivelse:</b> {{facility.description}}</p>
+      <p><b>Service(s):</b> {{ getServices }}</p>
+      <p><b>Brugerbetaling:</b> {{ getPayment }}</p>
+      <p><b>Beskrivelse:</b> {{ facility.description }}</p>
     </div>
 
     <div v-if="showMore" class="card-action">
       <div class="action-links d-flex align-items-center">
         <i class="icon icon-location-on mr-4"></i>
-        <a target="_blank" v-bind:href="`https://maps.apple.com/?daddr=${facility.city}+${facility.zip}+${facility.address}&dirflg=d`">{{facility.address}}, {{facility.city}}, {{facility.zip}}</a>
+        <a target="_blank"
+           v-bind:href="`https://maps.apple.com/?daddr=${facility.city}+${facility.zip}+${facility.address}&dirflg=d`">{{ facility.address }},
+          {{ facility.city }}, {{ facility.zip }}</a>
       </div>
     </div>
   </div>
@@ -59,6 +62,17 @@ export default {
     },
     getServices(): string {
       return this.facility.serviceTypes?.map((item: ListItem) => ServiceTypes[Number(item.value)])?.join(', ') ?? '';
+    },
+    getPayment(): string {
+
+      if (this.facility.paymentType == 0)
+        return "Nej";
+      if (this.facility.paymentType == 1)
+        return "Ja";
+      if (this.facility.paymentType == 2)
+        return "I nogen grad";
+      return "Ukendt";
+
     },
     getOrganisation(): string {
       return this.facility.organisation ?? '';
